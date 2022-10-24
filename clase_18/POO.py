d248c2d98e71be30bb3b4fe1f4ccbad71e08a32f
Julian Fernandez <julianfernandez1811@gmail.com>
2022-10-24 16:30:39 -0300

    introduccion a POO

---
+++ clase_18/POO.py
@@ -0,0 +1,101 @@
+class Personaje:
+    tipo = 'Personaje'  # Atributo de clase
+
+    def __init__(self, id, nombre, apellido, edad) -> None:
+        # Atributos del objeto
+        self.id = id
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.edad = edad
+        self._lista = [id, nombre, apellido, edad]
+
+
+    @property  # Getter
+    # Convierte una función (método) en una propiedad. Por eso, esta función despues se la debe invocar como variable.nombre
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter  # Setter
+    # Crea una propiedad que puede asignarle un valor a la property (key)
+    def set_nombre(self, value):
+        self.__nombre = value
+
+    # Metodo de instancia. Se lo printea como: print(instancia.descripcion())
+    def descripcion(self) -> str:
+        return '{0}_{1}'.format(self.__nombre, self.__apellido)
+
+    # Método especial: Mostrar objeto. Se lo printea como print(intancia)
+    def __str__(self) -> str:
+        return 'Nombre: {0}; Apellido: {1}'.format(self.__nombre, self.__apellido)
+
+    # Método especial: Permite utilizar la función len sobre una instancia de esta clase Personaje.
+    def __len__(self) -> str:
+        return self.edad
+
+    # Método especial: Permite llamar una lista dentro de la clase sin tener que llamar un método
+    def __getitem__(self, i):
+        return self._lista[i]
+
+    # Método especial: El mismo que el anterior pero permite setera un valor en una posición de una lista dentro de la clase
+    def __setitem__(self, i, value):
+        self._lista[i] = value
+
+    # Método especial: Consulta si un item se encuentra (in) en una lista de la clase. Devuelve un bool
+    def __contains__(self, value):
+        return value in self._lista
+
+    # Método especial: Itera sobre una lista
+    def __iter__(self):
+        for i in range(len(self._lista)):
+            yield self._lista[i] # retorna un objeto iterable (debe iterarse con un for por fuera)
+
+# Inheritance
+class Poderes(Personaje):
+
+    # def __init__(self, id, nombre, apellido, edad):
+    """def __init__(self, id, nombre, apellido, edad, poder):
+
+        Personaje.__init__(id, nombre, apellido, edad)  
+        self.poder = poder """
+
+    def __str__(self) -> str:
+        return 'Nombre: {0}; Poder: {1}'.format(self.__nombre, self.poder)
+
+
+# Asignar atributos a la clase - Instanciar
+personaje_A = Personaje(0, 'Marty', 'McFly', 18)
+# llamar a un método de la clase:
+# personaje_A.set_nombre = 'JORGEE'
+# print(personaje_A.descripcion()) # Marty
+
+# ------------- METODOS ESPECIALES ------------- #
+# __str__:
+# print(personaje_A)  # 'Nombre: Marty; Apellido: McFly'
+# método normal:
+# print(personaje_A.descripcion())  # 'Marty_McFly'
+
+# __len__:
+# print(len(personaje_A))  # '18'
+
+# __getitem__:
+# print(personaje_A[2])  # 'McFly'
+
+# __setitem__:
+# personaje_A[2] = 'McDown'
+# print(personaje_A[2]) #  'McDown'
+
+# __contains__:
+# print('Marty' in personaje_A) # True
+# print('Marta' in personaje_A) # False
+
+# __iter__:
+# for el in personaje_A:
+#     print(el)
+
+
+# ------------- Inheritance ------------- #
+# personaje_B = Poderes(1, 'Dr.', 'Sofenmacher', 18)
+# print(personaje_B)
+
+# print(personaje_A.__nombre)  # Intentar traer un atributo
+# OUTPUT: AttributeError: 'Personaje' object has no attribute '__nombre'
